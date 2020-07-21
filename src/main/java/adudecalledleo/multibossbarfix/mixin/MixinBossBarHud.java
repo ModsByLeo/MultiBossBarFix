58b3b8741a62f8f06c073924909dc5933ffc5383
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2020-07-21 22:27:08 +0300

    1.16 or something

--- src/main/java/adudecalledleo/multibossbarfix/mixin/MixinBossBarHud.java
+++ src/main/java/adudecalledleo/multibossbarfix/mixin/MixinBossBarHud.java
@@ -2,6 +2,7 @@ package adudecalledleo.multibossbarfix.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.hud.BossBarHud;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -9,22 +10,22 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(BossBarHud.class)
 public class MixinBossBarHud {
     @Redirect(method = "renderBossBar", at = @At(value = "INVOKE",
-                                                 target = "Lnet/minecraft/client/gui/hud/BossBarHud;blit(IIIIII)V",
+                                                 target = "Lnet/minecraft/client/gui/hud/BossBarHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V",
                                                  ordinal = 1))
-    private void multibossbarfix$blitOverlay(BossBarHud bossBarHud, int x, int y, int u, int v, int width, int height) {
+    private void multibossbarfix$blitOverlay(BossBarHud bossBarHud, MatrixStack matrixStack, int x, int y, int u, int v, int width, int height) {
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
-        bossBarHud.blit(x, y, u, v, width, height);
+        bossBarHud.drawTexture(matrixStack, x, y, u, v, width, height);
         RenderSystem.disableBlend();
     }
 
     @Redirect(method = "renderBossBar", at = @At(value = "INVOKE",
-                                                 target = "Lnet/minecraft/client/gui/hud/BossBarHud;blit(IIIIII)V",
+                                                 target = "Lnet/minecraft/client/gui/hud/BossBarHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V",
                                                  ordinal = 3))
-    private void multibossbarfix$blitOverlayFill(BossBarHud bossBarHud, int x, int y, int u, int v, int width, int height) {
+    private void multibossbarfix$blitOverlayFill(BossBarHud bossBarHud, MatrixStack matrixStack, int x, int y, int u, int v, int width, int height) {
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
-        bossBarHud.blit(x, y, u, v, width, height);
+        bossBarHud.drawTexture(matrixStack, x, y, u, v, width, height);
         RenderSystem.disableBlend();
     }
 }
